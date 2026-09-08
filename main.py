32edb7c42300834bd3eaf8deada7d9eb949bd8e0
Grace Jones <graciecircus@gmail.com>
2026-09-07 20:13:41 -0400

    Add tasks GET and POST

--- main.py
+++ main.py
@@ -47,3 +47,52 @@ def get_projects():
     connection.close()
 
     return [dict(project) for project in projects]
+
+@app.post("/projects/{project_id}/tasks")
+def create_task(
+    project_id: int,
+    title: str,
+    description: str = "",
+    priority: str = "Medium"
+):
+    connection = get_connection()
+
+    cursor = connection.execute(
+        """
+        INSERT INTO tasks (title, description, priority, project_id)
+        VALUES (?, ?, ?, ?)
+        """,
+        (title, description, priority, project_id)
+    )
+
+    connection.commit()
+
+    task_id = cursor.lastrowid
+
+    connection.close()
+
+    return {
+        "id": task_id,
+        "title": title,
+        "description": description,
+        "priority": priority,
+        "project_id": project_id
+    }
+
+
+@app.get("/projects/{project_id}/tasks")
+def get_tasks(project_id: int):
+    connection = get_connection()
+
+    tasks = connection.execute(
+        """
+        SELECT *
+        FROM tasks
+        WHERE project_id = ?
+        """,
+        (project_id,)
+    ).fetchall()
+
+    connection.close()
+
+    return [dict(task) for task in tasks]
